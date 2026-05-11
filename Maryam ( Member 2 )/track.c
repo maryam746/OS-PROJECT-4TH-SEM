a6ad157f4b5a050f59fbd0c90d5901b8f2f2087f
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2026-05-11 17:06:30 +0500

    Update track.c

--- Maryam ( Member 2 )/track.c
+++ Maryam ( Member 2 )/track.c
@@ -1,6 +1,6 @@
 #include "track.h"
 
-TrackSegment tracks[TOTAL_TRACKS];
+TrackSegment tracks[TOTAL_TRACKS];  //originial def yahaan pr baaki sari files will access using extern
 
 void initializeTracks() {
     for(int i = 0; i < TOTAL_TRACKS; i++) {
