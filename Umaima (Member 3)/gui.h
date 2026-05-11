028f770fb5764d8821be4af713b2782b54cb291f
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2026-05-11 21:04:19 +0500

    Update gui.h

--- Umaima (Member 3)/gui.h
+++ Umaima (Member 3)/gui.h
@@ -4,64 +4,55 @@
 #include "train.h"
 #include "raylib.h"
 
-// Constants
-#define SCREEN_WIDTH 1600
+#define SCREEN_WIDTH 1600   
 #define SCREEN_HEIGHT 850
 #define TRAIN_WIDTH 35
 #define TRAIN_HEIGHT 50
 #define TOTAL_TRACKS 5
-#define TOTAL_JUNCTIONS 8  // Increased to 8 junctions
+#define TOTAL_JUNCTIONS 8 
 #define SAFE_GAP (TRAIN_HEIGHT + 15)
 
 
-// Track positions structure
 typedef struct {
-    int x;           // X coordinate of the track
-    int y_start;     // Starting Y position
-    int y_end;       // Ending Y position
-    int track_id;    // Track number (1-5)
+    int x;         
+    int y_start;     
+    int y_end;    
+    int track_id;    //track no 1-5
 } TrackData;
 
-// Junction structure with Y position
 typedef struct {
-    int x, y;
-    int from_track;      // Track numbers (1-5)
-    int to_track;        // Track numbers (1-5)
+    int x, y;           //center point
+    int from_track;     
+    int to_track;     
     int occupied_by;     // train id that is using this junction
-    int y_position;      // Vertical position of this junction on the track
 } Junction;
 
-// Train visual data structure
+
 typedef struct {
     int train_id;
-    int current_track;      // Which track the train is on (1-5)
+    int current_track;     //train konse track pr hai
     float y_pos;            // Vertical position on the track
-    int moving;             // Is the train moving
-    int using_junction;     // Is train currently on a junction
-    int target_junction;    // Which junction it's heading to
-    int original_track;     // Original track before junction
+    int moving;             // 1=moving wrna 0
+    int using_junction;     // 1=usign wrna 0
+    int target_junction;    // junction id jo use kr rha hai
     int junction_progress;  // Progress through junction (0-100)
-    float junction_x;       // X position when on junction
-    int waiting_at_start;   // Is train waiting at start of track
+    int waiting_at_start;   // 1=waiting at top wrna 0
     float speed;            // Movement speed
-    int dest_track;         // Destination track
-    int junctions_used;     // Count of junctions used in current journey
-    int max_junctions;      // Max junctions to use (1-3)
+    int dest_track;         // end kahaan hoga
 } VisualTrain;
 
-// Function declarations
+
 void* startGUI(void* arg);
 void drawTracks();
 void drawJunctions();
 void drawTrain(int x, int y, int train_id, int is_moving, Color color, int waiting_at_start);
-Color getDullColor(Color c);
+Color getDullColor(Color c);        //for waiting
 
-// Global external declarations
 extern TrackData track_positions[TOTAL_TRACKS];
 extern Junction junctions[TOTAL_JUNCTIONS];
 extern VisualTrain visual_trains[10];
-extern int num_visual_trains;
-extern pthread_mutex_t visual_mutex;
+extern int num_visual_trains;           //current active trains
+extern pthread_mutex_t visual_mutex;        //for protecting visual_trains and num_visual_trains
 extern Color bright_colors[6];
 
 #endif
